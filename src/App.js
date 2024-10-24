fca4f83bcd7de016b0bd71bffacc306795472cb6
mafyyu <morita0126m@gmail.com>
2024-10-24 16:41:27 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+const users = [
+  {
+    id: 1,
+    name: 'Hedy Lamarr',
+    imgUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+    imgSize: 120,
+  },
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    imgUrl: 'https://i.imgur.com/SKnAwd5s.jpg',
+    imgSize: 120,
+  },
+  {
+    id: 3,
+    name: 'Grace Hopper',
+    imgUrl: 'https://i.imgur.com/1A9iV4H.jpg',
+    imgSize: 120,
+  }
+];
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 class="title">Introduction</h1>
+      <div>
+        {users.map((user)=>(
+          
+          <div key={user.id} class="card">
+            <h1>{user.name}</h1>
+            <img
+            src={user.imgUrl}
+            style={{
+              width:user.imgSize,
+              height:user.imgSize
+            }}>
+            </img>
+          </div>
+        ))}
+        
+        
+      </div>
     </div>
   );
 }
